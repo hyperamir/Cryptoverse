9831bd3ffb51b69c5a9b11a3a2638ad6ddc36e3e
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-03-16 13:49:25 -0400

    fetch data from rapidApi

---
+++ src/services/cryptoApi.js
@@ -0,0 +1,41 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+const cryptoApiHeaders = {
+  'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+  'x-rapidapi-key': 'b4eeba7c28msh25c04c0080b05cap10388ajsn8f27ebd00149'
+};
+
+const baseUrl = 'https://coinranking1.p.rapidapi.com';
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: 'cryptoApi',
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest(`/coins`),
+    })
+  })
+})
+
+export const { useGetCryptosQuery } = cryptoApi;
+
+
+
+// var options = {
+//   method: 'GET',
+//   url: 'https://coinranking1.p.rapidapi.com/coins',
+//   params: {
+//     referenceCurrencyUuid: 'yhjMzLPhuIDl',
+//     timePeriod: '24h',
+//     tiers: '1',
+//     orderBy: 'marketCap',
+//     orderDirection: 'desc',
+//     limit: '50',
+//     offset: '0'
+//   },
+//   headers: {
+//     'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
+//     'x-rapidapi-key': 'b4eeba7c28msh25c04c0080b05cap10388ajsn8f27ebd00149'
+//   }
+// };
