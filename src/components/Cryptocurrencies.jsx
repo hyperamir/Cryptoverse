7b37e7023454a60a50996ce20ef62ef8704a9d9e
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-03-17 14:05:49 -0400

    news component search bar

--- src/components/Cryptocurrencies.jsx
+++ src/components/Cryptocurrencies.jsx
@@ -4,22 +4,22 @@ import { Link } from 'react-router-dom';
 import { Card, Row, Col, Input } from 'antd';
 import { useGetCryptosQuery } from "../services/cryptoApi";
 
-const Cryptocurrencies = ({simplified}) => {
-  const count = simplified ? 10 : 100 ;
+const Cryptocurrencies = ({ simplified }) => {
+  const count = simplified ? 10 : 100;
   const [searchTerm, setSearchTerm] = useState('')
   const { data: cryptoList, isFetching } = useGetCryptosQuery(count);
   const [cryptos, setCryptos] = useState([]);
-
-  useEffect(()=> {
+  console.log(cryptoList)
+  useEffect(() => {
     const filteredData = cryptoList?.data?.coins.filter(coin => coin.name.toLowerCase().includes(searchTerm.toLocaleLowerCase()));
     setCryptos(filteredData);
   }, [cryptoList, searchTerm])
 
-  if(isFetching) return 'Loading...';
+  if (isFetching) return 'Loading...';
   return (
     <>
       {!simplified && <div className="search-crypto">
-        <Input placeholder="Search Cryptocurrency" onChange={(event) => setSearchTerm(event.target.value)}/>
+        <Input placeholder="Search Cryptocurrency" onChange={(event) => setSearchTerm(event.target.value)} />
       </div>}
       <Row gutter={[32, 32]} className="crypto-card-container">
         {cryptos?.map(currency => (
@@ -30,7 +30,7 @@ const Cryptocurrencies = ({simplified}) => {
                 extra={<img className="crypto-image" src={currency.iconUrl} />}
                 hoverable
               >
-                <p>Price: {millify(currency.price)}</p>
+                <p>Price: {currency.price > 10 ? millify(currency.price) : Number(currency.price).toFixed(7)}</p>
                 <p>Market Cap: {millify(currency.marketCap)}</p>
                 <p>Daily Change: {millify(currency.change)}</p>
               </Card>
