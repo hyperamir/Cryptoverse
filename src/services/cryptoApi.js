96224e9f55a7a594999618ae34eae98adc904985
Amir Sedaghat <amir.sedaghat@gmail.com>
2022-03-18 12:16:58 -0400

    clean up

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -4,7 +4,7 @@ const cryptoApiHeaders = {
   'x-rapidapi-host': process.env.REACT_APP_CRYPTO_RAPIDAPI_HOST,
   'x-rapidapi-key': process.env.REACT_APP_RAPIDAPI_KEY
 };
-
+console.log(process.env)
 const baseUrl = 'https://coinranking1.p.rapidapi.com';
 const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
 
